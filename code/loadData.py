82a9fb949331ba1b6dbf4afbac1412f0f8c0e958
xuanchen <98302650+yaslydia@users.noreply.github.com>
2024-01-03 17:32:32 -0800

    fux

--- code/loadData.py
+++ code/loadData.py
@@ -15,13 +15,13 @@ def load_recording(dir_name, f_name,  n_cl = 4):
 	labels_O1 = mat['Data'].labels_O1
 	labels_O2 = mat['Data'].labels_O2
 
-
+	
 	eeg_O1 = mat['Data'].eeg_O1
 	eeg_O2 = mat['Data'].eeg_O2
 	LOC = mat['Data'].E2
 	ROC =  mat['Data'].E1
-
-
+	
+	
 	LOC =  np.expand_dims( LOC, axis=-1)
 	LOC =  np.expand_dims( LOC, axis=-1)
 	ROC =  np.expand_dims( ROC, axis=-1)
@@ -30,8 +30,8 @@ def load_recording(dir_name, f_name,  n_cl = 4):
 	eeg_O1 = np.expand_dims(eeg_O1, axis=-1)
 	eeg_O2 = np.expand_dims(eeg_O2, axis=-1)
 	eeg_O2 = np.expand_dims(eeg_O2, axis=-1)
-
-
+	
+	
 	EOG = np.concatenate( ( LOC, ROC ), axis = 2 )
 	EEG = eeg_O1
 	targets_O1 = labels_O1
@@ -66,19 +66,19 @@ def load_data(data_dir, files_train, w_len = 200*2):
 	print(E1.shape)
 	data_train = []
 	targets_train =  []
-
+	
 	f_list = files_train
-
+	
 	# we need to add zeros to the tensor in the beginning and in end
 	padding = ((w_len, w_len), (0, 0), (0, 0))
-
-
+	
+	
 	N_samples = 0
 	for i in range(0,len(f_list)):
 		f = f_list[i]
 		print(f)
 		E1, E2,X2, targets1, targets2  = load_recording(data_dir, f )
-
+		
 		E1 = np.pad(E1, pad_width=padding, mode='constant', constant_values=0)
 		E2 = np.pad(E2, pad_width=padding, mode='constant', constant_values=0)
 		X2 = np.pad(X2, pad_width=padding, mode='constant', constant_values=0)
